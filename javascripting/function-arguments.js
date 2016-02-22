d78496022009c4a1e708afed60d87b66d8e9a07a
csantana <carlillos136@gmail.com>
2016-02-22 09:00:40 -0600

    First commit Added my Javascripting Solutions

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function math (uno, dos, tres)
+{
+	console.log(uno, dos, tres);
+}
+math(23, 34, 456);
---
+++ javascripting/functions.js
@@ -0,0 +1,7 @@
+function eat(food)
+{
+	return food + ' Taste good';
+}
+var bananas='bananas';
+console.log(eat(bananas));
+
